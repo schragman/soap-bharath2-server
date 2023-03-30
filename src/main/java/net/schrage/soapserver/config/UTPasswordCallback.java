0abf3ad581bf812e1c98216849be62bb32579889
schragman <schragman@gmail.com>
2023-03-30 06:19:06 +0200

    Installed Username Token Security

---
+++ src/main/java/net/schrage/soapserver/config/UTPasswordCallback.java
@@ -0,0 +1,30 @@
+package net.schrage.soapserver.config;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class UTPasswordCallback implements CallbackHandler {
+
+  private Map<String, String> storedPasswords = new HashMap<>();
+
+  public UTPasswordCallback() {
+    storedPasswords.put("Michael", "geheim");
+    storedPasswords.put("Lisa", "nochgeheimer");
+  }
+
+  @Override
+  public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+    Arrays.stream(callbacks)
+        .map(callback -> (WSPasswordCallback) callback)
+        .filter(pwCallback -> storedPasswords.containsKey(pwCallback.getIdentifier()))
+        .forEach(pwCallback -> pwCallback.setPassword(storedPasswords.get(pwCallback.getIdentifier())));
+  }
+}
--- src/main/java/net/schrage/soapserver/config/WebServiceConfig.java
+++ src/main/java/net/schrage/soapserver/config/WebServiceConfig.java
@@ -6,6 +6,9 @@ import jakarta.xml.ws.soap.SOAPBinding;
 import net.schrage.soapserver.ws.SumWsImpl;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.ws.security.wss4j.WSS4JInInterceptor;
+import org.apache.wss4j.common.ConfigurationConstants;
+import org.apache.wss4j.dom.WSConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,6 +28,17 @@ public class WebServiceConfig {
   public Endpoint endpoint() {
     EndpointImpl endpoint = new EndpointImpl(bus, new SumWsImpl());
     endpoint.publish("/sumservice");
+
+    Map<String, Object> inProps = new HashMap<>();
+
+    inProps.put(ConfigurationConstants.ACTION, ConfigurationConstants.USERNAME_TOKEN);
+    inProps.put(ConfigurationConstants.PASSWORD_TYPE, WSConstants.PW_TEXT);
+    inProps.put(ConfigurationConstants.PW_CALLBACK_CLASS, UTPasswordCallback.class.getName());
+
+
+
+    WSS4JInInterceptor wssIn = new WSS4JInInterceptor(inProps);
+    endpoint.getInInterceptors().add(wssIn);
 
     return endpoint;
   }
